6580a3bfa48990b72dc04661c0b555d112edda8f
mfarsx <arslantrkfatih@gmail.com>
2024-02-10 19:17:49 +1100

    Refactor fetchWrapper function and fix typo in header

--- src/providers/data/fetch-wrapper.ts
+++ src/providers/data/fetch-wrapper.ts
@@ -6,8 +6,7 @@ type Error = {
 };
 
 const customFetch = async (url: string, options: RequestInit) => {
-  const accessToken = localStorage.getItem("accessToken");
-
+  const accessToken = localStorage.getItem("access_token");
   const headers = options.headers as Record<string, string>;
 
   return await fetch(url, {
@@ -16,9 +15,23 @@ const customFetch = async (url: string, options: RequestInit) => {
       ...headers,
       Authorization: headers?.Authorization || `Bearer ${accessToken}`,
       "Content-Type": "application/json",
-      "Apollo-Require-Prelight": "true",
+      "Apollo-Require-Preflight": "true",
     },
   });
+};
+
+export const fetchWrapper = async (url: string, options: RequestInit) => {
+  const response = await customFetch(url, options);
+
+  const responseClone = response.clone();
+  const body = await responseClone.json();
+  const error = getGraphQLErrors(body);
+
+  if (error) {
+    throw error;
+  }
+
+  return response;
 };
 
 const getGraphQLErrors = (
@@ -33,30 +46,14 @@ const getGraphQLErrors = (
 
   if ("errors" in body) {
     const errors = body?.errors;
-
-    const message = errors?.map((error) => error?.message)?.join("\n");
+    const messages = errors?.map((error) => error?.message)?.join("");
     const code = errors?.[0]?.extensions?.code;
 
     return {
-      message: message || JSON.stringify(errors),
+      message: messages || JSON.stringify(errors),
       statusCode: code || 500,
     };
   }
 
   return null;
-};
-
-export const fetchWrapper = async (url: string, options: RequestInit) => {
-  const response = await customFetch(url, options);
-
-  const responseClone = response.clone();
-  const body = await responseClone.json();
-
-  const error = getGraphQLErrors(body);
-
-  if (error) {
-    throw error;
-  }
-
-  return response;
 };
