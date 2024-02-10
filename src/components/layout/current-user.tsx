7700c415c626c870826c9e6663416e538e373da3
mfarsx <arslantrkfatih@gmail.com>
2024-02-11 10:38:16 +1100

    Add AccountSettings component and integrate it with CurrentUser component

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,14 +1,42 @@
-import { Popover } from "antd";
+import { Button, Popover } from "antd";
 import CustomAvatar from "../custom-avatar";
 import { useGetIdentity } from "@refinedev/core";
+import { SettingOutlined } from "@ant-design/icons";
 
 import type { User } from "@/graphql/schema.types";
+import { Text } from "../text";
+import { useState } from "react";
+import { AccountSettings } from "./account-settings";
 
 const CurrentUser = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const { data: user } = useGetIdentity<User>();
 
   const content = (
-    <div style={{ display: "flex", flexDirection: "column" }}></div>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <Text strong style={{ padding: "12px 20px" }}>
+        {user?.name}
+      </Text>
+      <div
+        style={{
+          borderTop: "1px solid d9d9d9",
+          padding: "4px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "4px",
+        }}
+      >
+        <Button
+          style={{ textAlign: "left" }}
+          icon={<SettingOutlined />}
+          type="text"
+          block
+          onClick={() => setIsOpen(true)}
+        >
+          Account Settings
+        </Button>
+      </div>
+    </div>
   );
 
   return (
@@ -18,6 +46,7 @@ const CurrentUser = () => {
         trigger="click"
         overlayInnerStyle={{ padding: 0 }}
         overlayStyle={{ zIndex: 999 }}
+        content={content}
       >
         <CustomAvatar
           name={user?.name}
@@ -26,6 +55,13 @@ const CurrentUser = () => {
           style={{ cursor: "pointer" }}
         />
       </Popover>
+      {user && (
+        <AccountSettings
+          opened={isOpen}
+          setOpened={setIsOpen}
+          userId={user.id}
+        />
+      )}
     </>
   );
 };
